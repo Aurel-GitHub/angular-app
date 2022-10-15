1dc39aae8eae9b02d72c2ec7f0ce5ea1c588a067
aurelien_64 <aurelien.landouer@troov.com>
2022-10-15 10:47:51 +0200

    update expense interface & router exp by id

--- src/app/_interfaces/i-expenses.ts
+++ src/app/_interfaces/i-expenses.ts
@@ -2,13 +2,14 @@ import { IBudget } from './i-budgets';
 import { IUser } from './i-user';
 
 export interface IExpenses {
+  _id: string;
   title: string;
   total: string;
   isFixed: boolean;
   isVariable: boolean;
   hasPassed: boolean;
-  datePassed: boolean;
-  enDateIsFixed: boolean;
+  datePassed: Date;
+  enDateIsFixed: Date;
   commment: string;
   user: IUser;
   budget: IBudget;
